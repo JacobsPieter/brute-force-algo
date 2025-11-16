c19f9b0bdb6c57e1e1776c8a561d51d6c8ffe361
Pieter Jacobs <157968110+JacobsPieter@users.noreply.github.com>
2025-11-16 22:19:04 +0100

    works (kind of) need more time to test

--- run_all_combinations.py
+++ run_all_combinations.py
@@ -4,7 +4,8 @@ import numba.typed
 from numba import njit
 
 
-MAX_SKILL_POINTS = 100 + 390
+MAX_SKILL_POINTS = 595
+#MAX_STRREQ =
 
 def get_stat_to_optimize() -> str:
     stat = input('Enter the stat to optimize: ').strip().lower()
@@ -33,78 +34,81 @@ def get_required_stats_dict(stat_to_optimise: str) -> dict:
     return required_stats_dict
 
 
-def combine_stats(item1_stats: tuple[list[str], list[int]], item2_stats: tuple[list[str], list[int]]) -> tuple[list[str], list[int]]:
-    combined_stats: tuple[list[str], list[int]] = (['' for i in range(len(item1_stats[0])+len(item2_stats[0]))], [0 for i in range(len(item1_stats[1])+len(item2_stats[1]))])
-    index = 0
-    for i in range(len(item1_stats[0])):
-        combined_stats[0][index] = item1_stats[0][i]
-        if item2_stats[0].count(item1_stats[0][i]) == 0:
-            combined_stats[1][index] = item1_stats[1][i]
-        else:
-            index_in_item_2 = item2_stats[0].index(item1_stats[0][i])
-            combined_stats[1][index] = item1_stats[1][i] + item2_stats[1][index_in_item_2]
-        index += 1
-    for i in range(len(item2_stats[0])):
-        if item1_stats[0].count(item2_stats[0][i]) == 0:
-            combined_stats[0][index] = item2_stats[0][i]
-            combined_stats[1][index] = item2_stats[1][i]
-        index += 1
-    for i in range(len(combined_stats[0])-index):
-        combined_stats[0].pop()
-        combined_stats[1].pop()
-    index = 0
+def combine_stats(item1_stats: dict[str, int], item2_stats: dict[str, int]) -> dict[str, int]:
+    combined_stats: dict[str, int] = {}
+    for stat in set(item1_stats.keys()).union(item2_stats.keys()):
+        if item1_stats.get(stat) is None:
+            combined_stats[stat] = item2_stats.get(stat, 0)
+            continue
+        if item2_stats.get(stat) is None:
+            combined_stats[stat] = item1_stats.get(stat, 0)
+            continue
+        combined_stats[stat] = item1_stats.get(stat, 0) + item2_stats.get(stat, 0)
     return combined_stats
+
     
 
 
 
-def get_all_combinations(weapon_names, helmet_names, chestplate_names, legging_names, boot_names, ring_names, bracelet_names, necklace_names, weapon_data, helmet_data, chestplate_data, legging_data, boot_data, ring_data, bracelet_data, necklace_data, required_stats, best_amount):
-    best_combinations: list = []
-    for weapon_number in range(len(weapon_names)):
-        for helmet_number in range(len(helmet_names)):
-            wh_stats: tuple[list[str], list[int]] = combine_stats(weapon_data[weapon_number], helmet_data[helmet_number])
-            for chestplate_number in range(len(chestplate_names)):
-                whc_stats: tuple[list[str], list[int]] = combine_stats(wh_stats, chestplate_data[chestplate_number])
-                for legging_number in range(len(legging_names)):
-                    whcl_stats: tuple[list[str], list[int]] = combine_stats(whc_stats, legging_data[legging_number])
-                    for boot_number in range(len(boot_names)):
-                        whclb_stats: tuple[list[str], list[int]] = combine_stats(whcl_stats, boot_data[boot_number])
-                        for ring1_number in range(len(ring_names)):
-                            whclbr_stats: tuple[list[str], list[int]] = combine_stats(whclb_stats, ring_data[ring1_number])
-                            for ring2_number in range(len(ring_names)):
-                                whclbrr_stats: tuple[list[str], list[int]] = combine_stats(whclbr_stats, ring_data[ring2_number])
-                                for bracelet_number in range(len(bracelet_names)):
-                                    whclbrrba_stats: tuple[list[str], list[int]] = combine_stats(whclbrr_stats, bracelet_data[bracelet_number])
-                                    for necklace_number in range(len(necklace_names)):
-                                        whclbrrban_stats: tuple[list[str], list[int]] = combine_stats(whclbrrba_stats, necklace_data[necklace_number])
+def get_all_combinations(helmets: dict, chestplates: dict, leggings: dict, boots: dict, rings: dict, bracelets: dict, necklaces: dict, weapons: dict, required_stats: dict, best_amount):
+    best_combinations: dict[str, dict[str, int]] = {}
+    for weapon in weapons:
+        for helmet in helmets:
+            wh_stats: dict[str, int] = combine_stats(weapons[weapon], helmets[helmet])
+            if wh_stats.get('strReq', 0) + wh_stats.get('dexReq',0) + wh_stats.get('intReq', 0) + wh_stats.get('defReq',0) + wh_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                continue
+            for chestplate in chestplates:
+                whc_stats: dict[str, int] = combine_stats(wh_stats, chestplates[chestplate])
+                if whc_stats.get('strReq', 0) + whc_stats.get('dexReq',0) + whc_stats.get('intReq', 0) + whc_stats.get('defReq',0) + whc_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                    continue
+                for legging in leggings:
+                    whcl_stats: dict[str, int] = combine_stats(whc_stats, leggings[legging])
+                    if whcl_stats.get('strReq', 0) + whcl_stats.get('dexReq',0) + whcl_stats.get('intReq', 0) + whcl_stats.get('defReq',0) + whcl_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                        continue
+                    for boot in boots:
+                        whclb_stats: dict[str, int] = combine_stats(whcl_stats, boots[boot])
+                        if whclb_stats.get('strReq', 0) + whclb_stats.get('dexReq',0) + whclb_stats.get('intReq', 0) + whclb_stats.get('defReq',0) + whclb_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                            continue
+                        for ring1 in rings:
+                            whclbr_stats: dict[str, int] = combine_stats(whclb_stats, rings[ring1])
+                            if whclbr_stats.get('strReq', 0) + whclbr_stats.get('dexReq',0) + whclbr_stats.get('intReq', 0) + whclbr_stats.get('defReq',0) + whclbr_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                                continue
+                            for ring2 in rings:
+                                whclbrr_stats: dict[str, int] = combine_stats(whclbr_stats, rings[ring2])
+                                if whclbrr_stats.get('strReq', 0) + whclbrr_stats.get('dexReq',0) + whclbrr_stats.get('intReq', 0) + whclbrr_stats.get('defReq',0) + whclbrr_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                                    continue
+                                for bracelet in bracelets:
+                                    whclbrrba_stats: dict[str, int] = combine_stats(whclbrr_stats, bracelets[bracelet])
+                                    if whclbrrba_stats.get('strReq', 0) + whclbrrba_stats.get('dexReq',0) + whclbrrba_stats.get('intReq', 0) + whclbrrba_stats.get('defReq',0) + whclbrrba_stats.get('agiReq',0) > MAX_SKILL_POINTS:
+                                        continue
+                                    for necklace in necklaces:
+                                        whclbrrban_stats: dict[str, int] = combine_stats(whclbrrba_stats, necklaces[necklace])
+                                        final_stats: dict[str, int] = whclbrrban_stats
                                         possible = True
                                         sort_by_stat: str = ''
-                                        for stat in required_stats[0]:
-                                            if stat not in whclbrrban_stats[0]:
+                                        total_skill_req = final_stats.get('strReq', 0) + final_stats.get('dexReq',0) + final_stats.get('intReq', 0) + final_stats.get('defReq',0) + final_stats.get('agiReq',0)
+                                        if total_skill_req > MAX_SKILL_POINTS:
+                                            continue
+                                        for counter, stat in enumerate(required_stats):
+                                            if whclbrrban_stats.get(stat) is None:
                                                 possible = False
                                                 break
-                                            index_in_stats = whclbrrban_stats[0].index(stat)
-                                            stat_value = whclbrrban_stats[1][index_in_stats]
-                                            if required_stats[1][required_stats[0].index(stat)] is not None:
-                                                if stat_value < required_stats[1][required_stats[0].index(stat)]:
+                                            stat_value = whclbrrban_stats[stat]
+                                            if required_stats[stat] is not None:
+                                                if stat_value < required_stats[stat]:
                                                     possible = False
                                                     break
                                             else:
                                                 sort_by_stat = stat
                                         if possible:
-                                            combination_name = f'{weapon_names[weapon_number]} + {helmet_names[helmet_number]} + {chestplate_names[chestplate_number]} + {legging_names[legging_number]} + {boot_names[boot_number]} + {ring_names[ring1_number]} + {ring_names[ring2_number]} + {bracelet_names[bracelet_number]} + {necklace_names[necklace_number]}'
-                                            best_combinations.append((combination_name, whclbrrban_stats))
-                                            if len(best_combinations)>1:
-                                                best_combination_names = [comb[0] for comb in best_combinations]
-                                                best_combination_values = [comb[1] for comb in best_combinations]
-                                                new_best_combination_names, best_combination_values = sort.sort_by_value(best_combination_names,best_combination_values, sort_by_stat)
-                                                new_best_combinations = []
-                                                for i in range(len(best_combination_names)):
-                                                    new_best_combinations.append((new_best_combination_names[i], best_combinations[best_combination_names.index(new_best_combination_names[i])][1]))
-                                                best_combinations = new_best_combinations
-                                                #print(best_combination_names)
-                                            if len(best_combinations) > best_amount:
-                                                best_combinations = best_combinations[:best_amount]
+                                            combination_name = f'{weapon} + {helmet} + {chestplate} + {legging} + {boot} + {ring1} + {ring2} + {bracelet} + {necklace}'
+                                            best_combinations[combination_name] = whclbrrban_stats
+                                            if len(best_combinations)>best_amount:
+                                                names, stats = sort.sort_dict_by_value(best_combinations, sort_by_stat)
+                                                names.reverse()
+                                                stats.reverse()
+                                                names, stats = names[:best_amount], stats[:best_amount]
+                                                best_combinations.fromkeys(names, [stat for stat in stats])
                                             #comment to add a breakpoint
     try:
         return best_combinations
@@ -150,23 +154,13 @@ def get_all_combinations_wrapper(inputs):
 
 def main():
     item_lists = parse_items.parse_items()
-    helmet_names, helmet_data = listify_items(item_lists[0])
-    chestplate_names, chestplate_data = listify_items(item_lists[1])
-    legging_names, legging_data = listify_items(item_lists[2])
-    boot_names, boot_data = listify_items(item_lists[3])
-    ring_names, ring_data = listify_items(item_lists[4])
-    bracelet_names, bracelet_data = listify_items(item_lists[5])
-    necklace_names, necklace_data = listify_items(item_lists[6])
-    spear_names, spear_data = listify_items(item_lists[7])
-    bow_names, bow_data = listify_items(item_lists[8])
-    dagger_names, dagger_data = listify_items(item_lists[9])
-    wand_names, wand_data = listify_items(item_lists[10])
-    relik_names, relik_data = listify_items(item_lists[11])
-    weapon_names = spear_names + bow_names + dagger_names + wand_names + relik_names
-    weapon_data = spear_data + bow_data + dagger_data + wand_data + relik_data
-    required_stats = parse_items.make_lists_from_dicts(get_required_stats_dict(get_stat_to_optimize()))
+    helmets, chestplates, leggings, boots, rings, bracelets, necklaces, spears, bows, daggers, wands, reliks = item_lists[0], item_lists[1], item_lists[2], item_lists[3], item_lists[4], item_lists[5], item_lists[6], item_lists[7], item_lists[8], item_lists[9], item_lists[10], item_lists[11]
+    weapons = {**spears, **bows, **daggers, **wands, **reliks}
+    weapons = reliks['Olympic']
+    required_stats = get_required_stats_dict(get_stat_to_optimize())
+    #required_stats = parse_items.make_lists_from_dicts(get_required_stats_dict(get_stat_to_optimize()))
     #weapon_names, helmet_names, chestplate_names , legging_names, boot_names, ring_names, bracelet_names, necklace_names, weapon_data, helmet_data, chestplate_data, legging_data, boot_data, ring_data, bracelet_data, necklace_data = get_all_combinations_wrapper((weapon_names, helmet_names, chestplate_names, legging_names, boot_names, ring_names, bracelet_names, necklace_names, weapon_data, helmet_data, chestplate_data, legging_data, boot_data, ring_data, bracelet_data, necklace_data))
-    combinations = get_all_combinations(weapon_names, helmet_names, chestplate_names, legging_names, boot_names, ring_names, bracelet_names, necklace_names, weapon_data, helmet_data, chestplate_data, legging_data, boot_data, ring_data, bracelet_data, necklace_data, required_stats, best_amount=10)
+    combinations = get_all_combinations(helmets, chestplates, leggings, boots, rings, bracelets, necklaces, weapons, required_stats, best_amount=10)
 
 if __name__ == "__main__":
     main()
--- sort.py
+++ sort.py
@@ -1,21 +1,8 @@
 import numpy as np
 from numba import njit
 
-class Counter(int):
-    def __init__(self):
-        self.count: int = 0
-
-    def increment(self):
-        self.count += 1
-    
-    def set(self, value: int):
-        self.count = value
-
-    def get(self):
-        return self.count
 
 
-@njit
 def bubbleSort(str_arr: list[str], value_arr: list) -> tuple[list[str], list]:
     n = len(str_arr)
     for i in range(n):
@@ -25,23 +12,34 @@ def bubbleSort(str_arr: list[str], value_arr: list) -> tuple[list[str], list]:
                 value_arr[j], value_arr[j+1] = value_arr[j+1], value_arr[j]
                 str_arr[j], str_arr[j+1] = str_arr[j+1], str_arr[j]
                 swapped = True
-        if swapped == False:
-            break
+        """ if swapped == False:
+            break """
     return str_arr, value_arr
 
 def sort_by_value(keys: list[str], data: list[list], sort_by: str) -> tuple[list[str], list[int]]:
     filtered_data: list = []
     for item in data:
         filtered_data.append(item[1][item[0].index(sort_by)])
-    bubbleSort(keys, filtered_data)
     return bubbleSort(keys, filtered_data)
     
 
 
-
+def sort_dict_by_value(unsorted_dict: dict[str, dict[str, int]], sort_by) -> tuple[list[str], list[dict[str, int]]]:
+    keys = list(unsorted_dict.keys())
+    value_groups = list(unsorted_dict.values())
+    n = len(keys)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if value_groups[j][sort_by] > value_groups[j+1][sort_by]:
+                value_groups[j], value_groups[j+1] = value_groups[j+1], value_groups[j]
+                keys[j], keys[j+1] = keys[j+1], keys[j]
+                swapped = True
+        if swapped == False:
+            break
+    return keys, value_groups
 
 def sort_dict(unsorted_dict: dict[str, int]) -> tuple[list[str], list[int]]:
-    counter = Counter()
     keys_list = list(unsorted_dict.keys())
     values_list = list(unsorted_dict.values())
     sorted_keys, sorted_values = bubbleSort(keys_list, values_list)
